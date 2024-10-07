66e05eff2f3802c551d3f06470250c16b75ec150
Manish Chaulagain <mchaulagain2s@semo.edu>
2024-10-06 23:08:00 -0700

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDq9BnmumHmg11AIuTSMSoXw024DKpQg9U",
+  authDomain: "cloud-based-to-do-lists.firebaseapp.com",
+  projectId: "cloud-based-to-do-lists",
+  storageBucket: "cloud-based-to-do-lists.appspot.com",
+  messagingSenderId: "1050553276520",
+  appId: "1:1050553276520:web:5a87e9ac149fb558c403dd",
+  measurementId: "G-PMRYC0EG49"
+};
+export { firebaseConfig };
